92e0766691a7d68026f973135658c636658dff7c
michelle perez <mmorales@boomsourcing.com>
2021-04-17 17:41:54 -0600

    base ready

--- src/app/components/recipes/recipe-list/recipe-list.component.ts
+++ src/app/components/recipes/recipe-list/recipe-list.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Recipe } from '../recipe.model';
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.scss']
+  styleUrls: ['./recipe-list.component.scss'],
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'Steak',
+      'Special Grilled Steak',
+      'https://images.pexels.com/photos/3535383/pexels-photo-3535383.jpeg?cs=srgb&dl=pexels-kasumi-loffler-3535383.jpg&fm=jpg'
+    ),
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
